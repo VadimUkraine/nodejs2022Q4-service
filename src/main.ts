3c39644ce9904f363767b47e2882f9e1b64450ed
v.oleynik <v.oleynik@itransition.com>
2023-02-06 22:27:13 +0200

    feat: add swagger for OpenAPI

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import * as yaml from 'js-yaml';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
@@ -10,6 +14,12 @@ async function bootstrap() {
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
   app.enableCors();
+
+  const document = yaml.load(
+    readFileSync(join(__dirname, '..', 'doc', 'api.yaml'), 'utf8'),
+  ) as OpenAPIObject;
+
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
